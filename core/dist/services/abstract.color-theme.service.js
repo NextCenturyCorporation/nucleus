dd635ad36771af0c83ebd80568255333de0bf1eb
Thomas Schellenberg <thomas.schellenberg@nextcentury.com>
2019-11-01 16:48:40 -0400

    Removed individual dist folders (for now).

--- core/dist/services/abstract.color-theme.service.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var AbstractColorThemeService = /** @class */ (function () {
-    function AbstractColorThemeService() {
-    }
-    return AbstractColorThemeService;
-}());
-exports.AbstractColorThemeService = AbstractColorThemeService;
-//# sourceMappingURL=abstract.color-theme.service.js.map
