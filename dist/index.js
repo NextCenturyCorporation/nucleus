b80ee6e05d9b808034fe7ae765f1acf669b0823e
Thomas Schellenberg <thomas.schellenberg@nextcentury.com>
2019-12-12 09:33:31 -0500

    Added a new index file for core, visualizations, and wrappers.  Changed the NPM package 'main' and 'types' files to the new common index file.  Updated version to 0.2.2

---
+++ dist/index.js
@@ -0,0 +1,39 @@
+"use strict";
+/**
+ * Copyright 2019 Next Century Corporation
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+require("./core/core.util");
+require("./core/date.util");
+require("./core/components/aggregation.web-component");
+require("./core/components/element.web-component");
+require("./core/components/filter.web-component");
+require("./core/components/group.web-component");
+require("./core/components/search.web-component");
+require("./core/services/abstract.color-theme.service");
+require("./core/services/abstract.search.service");
+require("./core/services/connection.service");
+require("./core/services/filter.service");
+require("./core/services/search.service");
+require("./core/models/color");
+require("./core/models/config-option");
+require("./core/models/dataset");
+require("./core/models/filters");
+require("./visualizations/text-cloud/text-cloud.visualization");
+require("./visualizations/text-cloud/text-cloud.web-component");
+require("./wrappers/angular/core/common.angular-component");
+require("./wrappers/angular/core/filter.angular-component");
+require("./wrappers/angular/core/search.angular-component");
+require("./wrappers/angular/text-cloud/text-cloud.angular-component");
+//# sourceMappingURL=index.js.map
---
+++ index.ts
@@ -0,0 +1,38 @@
+/**
+ * Copyright 2019 Next Century Corporation
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import './core/core.util';
+import './core/date.util';
+import './core/components/aggregation.web-component';
+import './core/components/element.web-component';
+import './core/components/filter.web-component';
+import './core/components/group.web-component';
+import './core/components/search.web-component';
+import './core/services/abstract.color-theme.service';
+import './core/services/abstract.search.service';
+import './core/services/connection.service';
+import './core/services/filter.service';
+import './core/services/search.service';
+import './core/models/color';
+import './core/models/config-option';
+import './core/models/dataset';
+import './core/models/filters';
+import './visualizations/text-cloud/text-cloud.visualization';
+import './visualizations/text-cloud/text-cloud.web-component';
+import './wrappers/angular/core/common.angular-component';
+import './wrappers/angular/core/filter.angular-component';
+import './wrappers/angular/core/search.angular-component';
+import './wrappers/angular/text-cloud/text-cloud.angular-component';
+
